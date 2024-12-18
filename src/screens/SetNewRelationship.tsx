fbe0e19af59f2ee6e4dc433540bf7cccb9d457d8
DKhiem017 <100778307+DKhiem017@users.noreply.github.com>
2024-12-18 23:35:01 +0700

    Set New Relationship UI

--- src/screens/SetNewRelationship.tsx
+++ src/screens/SetNewRelationship.tsx
@@ -13,6 +13,7 @@ import {SetNewRelationshipScreenProps} from '../types/navigator.type';
 import {useState, useRef} from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import Search from '../components/search';
+import SearchBar from '../components/SearchBar';
 
 const back = require('../assets/images/back-vector.png');
 const home = require('../assets/images/home-icon.png');
@@ -22,6 +23,9 @@ const chevronDown = require('../assets/images/chevron-down.png');
 const SetNewRelationshipScreen = ({
   navigation,
 }: SetNewRelationshipScreenProps) => {
+
+  const handleOnSearch = () => {}
+  
   return (
     <SafeAreaView className="flex-1 bg-[#FEFDFD] items-center">
       <TouchableOpacity className="h-[35] w-[35] mt-[20] absolute justify-center items-center left-[15] bg-[#2C7CC133] rounded-full">
@@ -31,7 +35,26 @@ const SetNewRelationshipScreen = ({
         <Text className=" leading-[0]-  text-2xl font-[600] text-[#2C7CC1]">
           New Relationship
         </Text>
-        <View className="h-[150] w-[150] rounded border mt-[15]" />
+        <View className="w-full items-center">
+          <View className="h-[150] w-[150] rounded border mt-[15]"></View>
+          <Text className="text-base font-[600] text-[#2C7CC1]">
+            Scan QR to set relationship with me
+          </Text>
+          {/* copy QR button */}
+          <TouchableOpacity className="py-[8] mt-[10] px-[31] bg-[#2C7CC1] rounded">
+            <Text className="text-[#fff] font-interBold ">Copy QR</Text>
+          </TouchableOpacity>
+        </View>
+        <View className="w-full mt-[28] ">
+          <Text className="font-interBold text-[#2C7CC1] text-lg">
+            Invite through numberphone
+          </Text>
+          <View className='w-full mt-[15]'>
+            <SearchBar placeholder='Search by name' onSearch={handleOnSearch}></SearchBar>
+          </View>
+          {/* list user */}
+          
+        </View>
       </View>
     </SafeAreaView>
   );
